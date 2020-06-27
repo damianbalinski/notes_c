601d2a3d3e23cbf34c7560575837f98740a4db54
gumises <gumis5151@gmail.com>
2020-06-27 22:32:21 +0200

    predefiniowane identyfikatory, nazwa fukcji

--- notatki/predefined_macros.c
+++ notatki/predefined_macros.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void main()
+int main()
 {
 	printf("%d\n", __LINE__);
 	printf("%s\n", __FILE__);
 	printf("%s\n", __DATE__);
 	printf("%s\n", __TIME__);
-	printf("%d\n", __STDC__);
 	printf("%d\n", __STDC_HOSTED__);
-	printf("%d\n", __STDC_VERSION__);
-	// printf("%d\n", __STDC_SECURE_LIB__);
-	// printf("%d\n", __STDC_WANT_SECURE_LIB__);
-	// printf("%s\n", __FUNCDNAME__);
+	printf("%d\n", __STDC_SECURE_LIB__);
+	printf("%d\n", __STDC_WANT_SECURE_LIB__);
+	// printf("%d\n", __STDC__);			// GCC
+	//printf("%d\n", __STDC_VERSION__);		// GCC
+	putchar('\n');
+
+	printf("%s\n", __FUNCDNAME__);
+	printf("%s\n", __FUNCTION__);
+	printf("%s\n", __FUNCSIG__);
+	//printf("%s\n", __PRETTY_FUNCTION__);	// GCC
 	printf("%s\n", __func__);
+
+	return 0;
 }
