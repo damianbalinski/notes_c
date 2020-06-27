80290d0fc43ede597ae27a720436ecb0bfa41cda
gumises <gumis5151@gmail.com>
2020-06-27 23:57:24 +0200

    wybór generyczny, dyrektywa pragma

--- notatki/generic.c
+++ notatki/generic.c
@@ -1,14 +1,59 @@
 #include <stdio.h>
 
-#define MYTYPE(X) _Generic((X),\
- int: "int",\
- float : "float",\
- double: "double",\
- default: "other"\
- )
+#define MYTYPE(X)_Generic((X),\
+    unsigned char       : "unsigned char", \
+    signed char         : "signed char", \
+    char                : "char", \
+    unsigned short      : "unsigned short", \
+    signed short        : "short", \
+    unsigned int        : "unsigned int",\
+    signed int          : "int",\
+    unsigned long       : "unsigned long", \
+    signed long         : "long", \
+    unsigned long long  : "unsigned long long", \
+    long long           : "long long", \
+    float               : "float",\
+    double              : "double",\
+    long double         : "long double", \
+    default             : "OTHER" \
+)
 
 void main()
 {
-    int a = 5;
-    printf("%s\n", MYTYPE(a));
+    unsigned char a1;
+    signed char a2;
+    char a3;
+
+    unsigned short b1;
+    short b2;
+
+    unsigned int c1;
+    int c2;
+
+    unsigned long d1;
+    long d2;
+
+    unsigned long long e1;
+    long long e2;
+
+    printf("%s\n", MYTYPE(a1));
+    printf("%s\n", MYTYPE(a2));
+    printf("%s\n", MYTYPE(a3));
+    printf("%s\n", MYTYPE(b1));
+    printf("%s\n", MYTYPE(b2));
+    printf("%s\n", MYTYPE(c1));
+    printf("%s\n", MYTYPE(c2));
+    printf("%s\n", MYTYPE(d1));
+    printf("%s\n", MYTYPE(d2));
+
+    double temp;
+
+    int res = _Generic(temp,
+        char: 0,
+        int : 1,
+        float : 2,
+        default: 3
+    );
+
+    printf("res: %d\n", res);
 }
---
+++ notatki/pragma.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#pragma STDC FENV_ACCESS DEFAULT
+#pragma STDC FP_CONTRACT DEFAULT
+#pragma STDC CX_LIMITED_RANGE DEFAULT
+
+int main()
+{
+	return 0;
+}
